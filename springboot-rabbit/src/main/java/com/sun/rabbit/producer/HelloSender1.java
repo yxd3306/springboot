8bdea7df0e6440b3f6c96bc698d4147bc0ed4b53
yxd <jyxd1997@163.com>
2019-05-17 14:29:54 +0800

    springboot整合rabbitMQ

---
+++ springboot-rabbit/src/main/java/com/sun/rabbit/producer/HelloSender1.java
@@ -0,0 +1,28 @@
+package com.sun.rabbit.producer;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class HelloSender1 {
+
+    @Autowired
+    private AmqpTemplate rabbitTemplate;
+
+    public void send() {
+        String sendMsg = "hello1 " + new Date();
+        System.out.println("Sender1 : " + sendMsg);
+        this.rabbitTemplate.convertAndSend("helloQueue", sendMsg);
+    }
+
+    public void send(String msg) {
+        String sendMsg = msg + new Date();
+        System.out.println("Sender1 : " + sendMsg);
+        this.rabbitTemplate.convertAndSend("helloQueue", sendMsg);
+    }
+
+
+}
